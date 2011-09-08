b26d82812d86195e51e96fdb08bb40a48536878b
Emil Cardell <emil.cardell@gmail.com>
2011-09-08 16:20:51 +0200

    First commit

---
+++ Source/JuniorMVC3Test/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JuniorMVC3Test.Models;
+
+namespace JuniorMVC3Test.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Display(ProductInputModel productInputModel)
+        {
+            var productModel = new ProductOutputModel();
+            productModel.Id = 1;
+            productModel.Name = "Docka";
+
+            return View(productModel);
+        }
+
+        public ActionResult ListAll()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(ProductInputModel inputModel)
+        {
+            var productModel = new ProductOutputModel();
+            productModel.Id = 1;
+            productModel.Name = inputModel.Name;
+            return View(productModel);
+        }
+
+    }
+}
---
+++ Source/JuniorMVC3Test/Models/ProductOutputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace JuniorMVC3Test.Models
+{
+    public class ProductOutputModel
+    {
+        public ProductOutputModel()
+        {
+            RelateradeProdukter = new List<string>() { "Hatt", "Hus", "Hund" };
+        }
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<string> RelateradeProdukter { get; set; }
+        public string BildUrl { get; set; }
+        public bool ShowRelatedProductList { get { return RelateradeProdukter.Count > 0; } }
+    }
+}
